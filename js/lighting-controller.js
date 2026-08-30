ffd75e4151a8d5a12c36271ff6916b5cc2b49eef
Srinivas5773 <podilasrinivas902@gmail.com>
2026-08-30 13:35:06 +0530

    feat(3d): add WebGL shader preset configs and scene lighting controller

---
+++ js/lighting-controller.js
@@ -0,0 +1,28 @@
+﻿/**
+ * 3D WebGL Scene Lighting and Ambient Controller
+ * Configures dynamic lights and material reflectivity for 3D products.
+ */
+
+class LightingController {
+  constructor() {
+    this.presets = {
+      studio: { ambient: 0xffffff, intensity: 1.2, directional: 0xffeedd },
+      neon: { ambient: 0x220033, intensity: 2.0, directional: 0x00ffff },
+      sunset: { ambient: 0xffaa44, intensity: 1.5, directional: 0xff5533 }
+    };
+    this.currentPreset = 'studio';
+  }
+
+  getPreset(name) {
+    return this.presets[name] || this.presets.studio;
+  }
+
+  setPreset(name) {
+    if (this.presets[name]) {
+      this.currentPreset = name;
+    }
+    return this.getPreset(this.currentPreset);
+  }
+}
+
+module.exports = new LightingController();
